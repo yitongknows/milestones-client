0c3a73d88b5f186d2fcedcd7d24893f8062a246f
yitongknows <61811966+yitongknows@users.noreply.github.com>
2021-09-04 16:51:52 -0400

    using useState hook

--- src/home.js
+++ src/home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+    //let name = "mario";
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(25);
+
+    const handleClick = (e) => {
+        setName("luigi");
+        setAge(30);
+    };
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <p>
+                {name} is {age} years old
+            </p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 };
