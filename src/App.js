5e600e028c28f25a26bf397107433d28085c34ab
yitongknows <61811966+yitongknows@users.noreply.github.com>
2021-09-04 16:13:38 -0400

    created multiple components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./home";
 
 function App() {
+    const title = "This home page currently has nothing in it";
+    const likes = 50;
+
     return (
         <div className="App">
+            <Navbar />
             <div className="content">
-                <h1>This home page current has nothing in it</h1>
+                <Home />
             </div>
         </div>
     );
